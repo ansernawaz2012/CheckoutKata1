6112383a489ab39b466a0bb5f7d6538b5f827215
ansernawaz <42109592+ansernawaz@users.noreply.github.com>
2024-12-22 23:28:00 +0000

    Updated special prices for item A

--- CheckoutKata1/Class1.cs
+++ CheckoutKata1/Class1.cs
@@ -10,10 +10,34 @@
         { "C", 20 },
         { "D", 15 }
     };
+        private readonly Dictionary<string, (int quantity, int price)> specialPrices = new Dictionary<string, (int, int)>
+    {
+        { "A", (3, 130) },
+        { "B", (2, 45) }
+    };
+        private readonly Dictionary<string, int> itemCounts = new Dictionary<string, int>();
 
         public void Scan(string item)
         {
             if (prices.ContainsKey(item))
+            {
+                if (!itemCounts.ContainsKey(item))
+                {
+                    itemCounts[item] = 0;
+                }
+                itemCounts[item]++;
+                ApplyPricing(item);
+            }
+        }
+
+        private void ApplyPricing(string item)
+        {
+            if (specialPrices.ContainsKey(item) && itemCounts[item] >= specialPrices[item].quantity)
+            {
+                totalPrice = specialPrices[item].price;
+                itemCounts[item] -= specialPrices[item].quantity;
+            }
+            else
             {
                 totalPrice += prices[item];
             }
@@ -24,6 +48,7 @@
             return totalPrice;
         }
     }
+
 
 
 }
